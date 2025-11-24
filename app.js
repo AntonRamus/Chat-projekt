507761dbaaeb15ab0d79fa4c9ebeee773cd32e81
Anton Ramus <anton.ramus@hotmail.com>
2025-11-24 15:14:40 +0100

    v0.14.1

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-import express from 'express'
-import session from 'express-session'
-import fs from 'node:fs/promises'
+import express, { response } from "express";
+import session from "express-session";
+import fs from "node:fs/promises";
+import path from "node:path";
 
-const app = express()
-const userAmount = 3
-app.set('view engine', 'pug')
-app.use(express.static('assets'))
-app.use(express.json())
-app.use('/images', express.static('images'))
+const app = express();
+const userAmount = 3;
+app.set("view engine", "pug");
+app.use(express.static("assets"));
+app.use(express.json());
+app.use("/images", express.static("images"));
 
-app.use(session({
-    secret: '82CE19E6-1E02-450C-B71F-E29393A209BA',
+app.use(
+  session({
+    secret: "82CE19E6-1E02-450C-B71F-E29393A209BA",
     resave: true,
     saveUninitialized: true
 }))
@@ -39,83 +41,85 @@ class message {
 }
 
 async function loadUser(path) {
-    try {
-        const data = await fs.readFile(path, { encoding: 'utf8' })
-        return data.json()
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const data = await fs.readFile(path, { encoding: "utf8" });
+    return data.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function loadAllUsers() {
-    try {
-        const content = await fs.readdir('./users')
-        let users = []
-        for (let index = 0; index < content.length; index++) {
-            const data = await fs.readFile('./users/' + content[index], { encoding: 'utf8' });
-            const user = JSON.parse(data)
-            users[user.id] = user
-        }
-        return users
-    } catch (error) {
-        console.log(error)
+  try {
+    const content = await fs.readdir("./users");
+    let users = [];
+    for (let index = 0; index < content.length; index++) {
+      const data = await fs.readFile("./users/" + content[index], {
+        encoding: "utf8",
+      });
+      const user = JSON.parse(data);
+      users[user.id] = user;
     }
+    return users;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-let users = await loadAllUsers()
+let users = await loadAllUsers();
 
-app.get('/', async (request, response) => {
-    if (request.session.ok) {
-        //console.log(JSON.stringify(request.session))
-        response.render('frontpage', { title: 'Chatten', bruger: request.session.username, userlevel: request.session.userlevel })
-    } else {
-        response.render('login')
-    }
-})
+app.get("/", async (request, response) => {
+  if (request.session.ok) {
+    //console.log(JSON.stringify(request.session))
+    response.render("frontpage", {
+      title: "Chatten",
+      bruger: request.session.username,
+      userlevel: request.session.userlevel,
+    });
+  } else {
+    response.render("login");
+  }
+});
 
 async function findBruger(brugernavn) {
-    //console.log("findBruger: "+ users)
-    for (let user of users) {
-        if (brugernavn === user.brugernavn)
-            return user
-    }
-    console.log(brugernavn + ' ikke fundet')
-    return 'bruger ikke fundet'
+  //console.log("findBruger: "+ users)
+  for (let user of users) {
+    if (brugernavn === user.brugernavn) return user;
+  }
+  console.log(brugernavn + " ikke fundet");
+  return "bruger ikke fundet";
 }
 
-app.get('/login', (request, response) => {
-    response.render('login')
-})
+app.get("/login", (request, response) => {
+  response.render("login");
+});
 
+app.post("/login", async (request, response) => {
+  const brugernavn = request.body.username;
 
-
-app.post('/login', async (request, response) => {
-    const brugernavn = request.body.username
-
-    if (request.session.ok == 'undefined' || brugernavn == 'undefined') {
-        response.render('login')
+  if (request.session.ok == "undefined" || brugernavn == "undefined") {
+    response.render("login");
+  }
+  try {
+    const bruger = await findBruger(brugernavn);
+    //console.log("LoginPost: " + users)
+    if (users.includes(bruger)) {
+      const password = request.body.password;
+      if (bruger.password === password) {
+        request.session.ok = true;
+        request.session.username = brugernavn;
+        request.session.userlevel = bruger.brugerniveau;
+        response.status(201).send({ ok: true });
+      } else {
+        response.sendStatus(401);
+      }
+    } else {
+      response.sendStatus(401);
     }
-    try {
-        const bruger = await findBruger(brugernavn)
-        //console.log("LoginPost: " + users)
-        if (users.includes(bruger)) {
-            const password = request.body.password
-            if (bruger.password === password) {
-                request.session.ok = true
-                request.session.username = brugernavn
-                request.session.userlevel = bruger.brugerniveau
-                response.status(201).send({ ok: true })
-            } else {
-                response.sendStatus(401)
-            }
-        } else {
-            response.sendStatus(401)
-        }
-    } catch (error) {
-        response.sendStatus(401) //Unauthorized
-    }
-
-})
+  } catch (error) {
+    response.sendStatus(401); //Unauthorized
+  }
+});
 
 app.get('/opretUser', (request, response) => {
     if (request.session.userlevel == '3')
@@ -124,36 +128,35 @@ app.get('/opretUser', (request, response) => {
         response.sendStatus(401)
 })
 
-app.get('/logout', (request, response) => {
-    request.session.destroy((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            response.redirect('/');
-        }
-    });
-}
-);
+app.get("/logout", (request, response) => {
+  request.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.redirect("/");
+    }
+  });
+});
 
 async function getAllChats() {
-    const chatNames = await fs.readdir('./chats')
-    const chats = []
-    for (let chatName of chatNames) {
-        chats.push(JSON.parse(await fs.readFile('./chats/' + chatName)))
-    }
-    return chats
+  const chatNames = await fs.readdir("./chats");
+  const chats = [];
+  for (let chatName of chatNames) {
+    chats.push(JSON.parse(await fs.readFile("./chats/" + chatName)));
+  }
+  return chats;
 }
 
-app.get('/chats', async (request, response) => {
-    const userlevel = request.session.userlevel
+app.get("/chats", async (request, response) => {
+  const userlevel = request.session.userlevel;
 
-    if (userlevel < 1 || userlevel > 3) {
-        response.sendStatus(401) //Unauthorized
-    } else {
-        const chats = await getAllChats()
-        response.send(JSON.stringify(chats))
-    }
-})
+  if (userlevel < 1 || userlevel > 3) {
+    response.sendStatus(401); //Unauthorized
+  } else {
+    const chats = await getAllChats();
+    response.send(JSON.stringify(chats));
+  }
+});
 
 app.get('/chats/:id', async (request, response) => {
     const idToGet = request.params.id
@@ -309,5 +312,5 @@ app.post('/opretUser', async (request, response) => {
 })
 
 app.listen(9090, () => {
-    console.log('Listening on port 9090')
-})
+  console.log("Listening on port 9090");
+});
